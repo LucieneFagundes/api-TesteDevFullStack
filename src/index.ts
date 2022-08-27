e84b9d820b3957afd4426aef1d72f930d2e6d6dd
LucieneFagundes <lfnascimento99@gmail.com>
2022-08-27 20:58:01 -0300

    update cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
