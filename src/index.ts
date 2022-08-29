95c6ea2cbe57864ac1a0278983a552178d390baa
LucieneFagundes <lfnascimento99@gmail.com>
2022-08-29 16:07:01 -0300

    add app use

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,13 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { routes } from "./routes";
 import admin from "firebase-admin";
 import serviceAccount from "../serviceAccount.json";
-import { cert } from "firebase-admin/app";
-// import { initializeApp } from "firebase/app";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-// export const firebaseApp = initializeApp({
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-//   appId: process.env.APP_ID,
-// });
 
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -28,6 +16,21 @@ admin.initializeApp({
     projectId: serviceAccount.project_id,
   }),
 });
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error){
+      return response.status(400).json({
+          error: err.message
+      })
+  }
+
+  return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error"
+  })
+  
+});
+
 
 app.use(routes);
 
