dfd7bde20494788edba7a82756ddcdb945099cb4
LucieneFagundes <lfnascimento99@gmail.com>
2022-08-29 15:51:42 -0300

    cors temporario

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
 
 // export const firebaseApp = initializeApp({
 //   apiKey: process.env.API_KEY,
